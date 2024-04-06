9d13cee137d5cd42f71503abd393bfe8a45c00fc
Monitor_PC <icowbiyi@gmail.com>
2024-04-06 18:40:38 +0300

    !

--- go.sh
+++ go.sh
@@ -3,16 +3,16 @@
 USER_DIR=/home/munir
 DOTS_DIR=$USER_DIR/dots
 
-foo () {
+func () {
   rm -rf $USER_DIR/.config/$1
   ln -s $DOTS_DIR/configs/$1 $USER_DIR/.config
 }
 
-foo "rofi"
-foo "kitty"
-foo "i3"
-foo "fish"
-foo "helix"
+func "berry"
+func "rofi"
+func "kitty"
+func "fish"
+func "helix"
 
 # if [ -d $USER_DIR/.config/Code/User/ ]; then
   # ln -sf $DOTS_DIR/vscode/settings.json $USER_DIR/.config/Code/User/
@@ -23,5 +23,5 @@ ln -sf $DOTS_DIR/x11/user-dirs.dirs $USER_DIR/.config/
 ln -sf $DOTS_DIR/x11/.xinitrc $USER_DIR/
 # ln -sf $DOTS_DIR/misc/config $USER_DIR/.ssh/
 
-cp $DOTS_DIR/x11/30-touchpad.conf /etc/X11/xorg.conf.d/
-
+cp -f $DOTS_DIR/x11/30-touchpad.conf /etc/X11/xorg.conf.d/
+cp -f $DOTS_DIR/x11/00-keyboard.conf /etc/X11/xorg.conf.d/
