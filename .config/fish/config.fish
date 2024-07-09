da02d6f396203e0cee3a629aaa261a87bff34d03
monitor <icowbiyi@gmail.com>
2024-07-10 02:06:27 +0300

    necessary stuf

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    alias hx "helix"
+    alias se "sudoedit"
+    alias b "cat /sys/class/power_supply/BAT1/capacity"
+    alias lock "i3lock"
+
+    alias jl "jupyter-lab"
+    alias python_activate "source ~/python_venv/bin/activate.fish"
+
+    set fish_greeting ""
+
+end
